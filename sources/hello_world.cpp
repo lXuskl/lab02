63bf3709a98d3c3bdf13cf7e4ff9aa4e1432a7a9
lXuskl <91723557+lXuskl@users.noreply.github.com>
2022-03-21 23:03:36 +0300

    Update hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-
-int main(int argc, char** argv)
-{
-    std::string name; // Инициальзируем переменную name типа string
-    std::cout << "Input user_name"; // Просим ввести имя пользователя
-    std::cin >> name; // Запрашиваем значение переменной name из потока ввода вывода
-    std::cout << "Hello world from " << name << std::endl; // Выводим преветствие
-    return 0;
-}
 
