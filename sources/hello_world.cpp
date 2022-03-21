22f2f91707eb441802126b66137c97f2a6897e9d
lXuskl <stalkercomiunity228@gmail.com>
2022-03-21 22:55:48 +0300

    Changed hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(int argc, char** argv)
 {
-cout << "Hello world" << endl;
+string name;
+cout << "Input user_name";
+cin >> name;
+cout << "Hello world from " << name << endl;
 return 0;
 }
