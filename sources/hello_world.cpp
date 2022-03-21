4b577263272d680ba31d668ca372498fe50940d0
lXuskl <91723557+lXuskl@users.noreply.github.com>
2022-03-21 23:09:14 +0300

    Update hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    std::string name;
+    std::cout << "Input user_name";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;
+}
 
