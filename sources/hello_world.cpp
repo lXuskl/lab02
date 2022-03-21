1d517d55eb53c8b713fd8cf7dc4772d8f52aa3f3
lXuskl <stalkercomiunity228@gmail.com>
2022-03-22 00:19:44 +0300

    l

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-string name;
-cout << "Input user_name";
-cin >> name;
-cout << "Hello world from " << name << endl;
-return 0;
+  string name;
+  cout << "Input user_name";
+  cin >> name;
+  cout << "Hello world from " << name << endl;
+  return 0;
 }
